43af041f1f4f6cca8d8fd8ffac5cfb9381e48d83
Abelardjohn <abelardpangalila@gmail.com>
2022-12-13 20:00:00 +0800

    secure path user dan admin

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,6 +10,8 @@
     <!-- <script src="https://kit.fontawesome.com/41038b9160.js" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="<?= base_url('/assets/fontawesome/css/all.css') ?>">
 
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+
     <!-- Styling references -->
     <link rel="stylesheet" href="<?= base_url('/assets/css/general.css') ?>">
     <link rel="stylesheet" href="<?= base_url('/assets/css/home.css') ?>">
